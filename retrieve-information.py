e75980eee13f756ce821429b7a38f6c63f2f5887
Koos van Strien <koos.van.strien@sigmadata.nl>
2024-02-19 20:25:54 +0100

    Small extension to: * store data (in XML files) * capture the right reports (and no others) * iterate over multpile stocks.

--- retrieve-information.py
+++ retrieve-information.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 import nest_asyncio
 import yfinance as yf
@@ -10,16 +11,31 @@ ib.connect('127.0.0.1', 7496, clientId = 3)
 
 def get_fundamentals_for(stock_ticker, market = 'SMART', currency = 'USD'):
     contract = Stock(stock_ticker, market, currency)
-    reportsToRequest = ['ReportsFinSummary', 'ReportsOwnership', 'ReportSnapshot', 'ReportsFinStatements', 'RESC', 'CalendarReport']
-    reports = [ib.reqFundamentalData(contract, report) for report in reportsToRequest]
+    reportsToRequest = ['ReportsFinStatements', 'ReportSnapshot'] # ['ReportsFinSummary', 'ReportsOwnership', 'ReportSnapshot', 'ReportsFinStatements', 'RESC']
+    reports = { report: ib.reqFundamentalData(contract, report) for report in reportsToRequest }
     return reports
 
 
 
-# Get the company's most important competitors from Yahoo
-print(get_competitors_for('JPM'))
 # Request the company's fundamentals
 # fundamentals = ib.reqFundamentalData(contract, 'ReportsFinStatements')
 
+companies_to_get = ['JPM']
+
+# Create folder "fundamentals" if it doesn't exist
+if not os.path.exists('fundamentals'):
+    os.makedirs('fundamentals')
+
+
+for company in companies_to_get:
+    fund = get_fundamentals_for(company)
+
+    # Create folder with company name if it doesn't exist
+    if not os.path.exists(f'./fundamentals/{company}'):
+        os.makedirs(f'./fundamentals/{company}')
+
+    for r in fund.keys():
+        with open(f'./fundamentals/{company}/{r}.xml', 'w') as file:
+            file.write(str(fund[r]))
 
 ib.disconnect()
