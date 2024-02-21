870b4ea0536cdef0091335b84588e6068df37c2e
Koos van Strien <koos.van.strien@wortell.nl>
2024-02-21 14:22:28 +0100

    Werk aan de helper - maar liever even parkeren.

--- process-xml.py
+++ process-xml.py
@@ -27,14 +27,58 @@ def process_RESC(xml_file):
     # 4. Actuals
     # 5. Consensus estimates (fiscal year; net profit)
 
+    """
+    1. Process security info
+    """
+    def process_security_info(tree):
+        security_els = tree.findall('Company/SecurityInfo/Security')
+        security_mappings = {
+            'values': {
+                'ISIN': f"SecIds/SecId[@type='ISIN']",
+                'RIC': f"SecIds/SecId[@type='RIC']",
+                'TICKER': f"SecIds/SecId[@type='TICKER']",
+                'InstrumentPI': f"SecIds/SecId[@type='InstrumentPI']",
+                'CLPRICE': f"MarketData/MarketDataItem[@type='CLPRICE']",
+                'MARKETCAP': f"MarketData/MarketDataItem[@type='CLPRICE']",
+                '52WKHIGH': f"MarketData/MarketDataItem[@type='CLPRICE']",
+                '52WKLOW': f"MarketData/MarketDataItem[@type='CLPRICE']", 
+            }, 
+            'attributes': {
+                'CLPRICE_Unit': (f"MarketData/MarketDataItem[@type='CLPRICE']", 'unit'),
+                'CLPRICE_CurrCode': (f"MarketData/MarketDataItem[@type='CLPRICE']", 'currCode'),
+                'MARKETCAP_Unit': (f"MarketData/MarketDataItem[@type='MARKETCAP']", 'unit'),
+                'MARKETCAP_CurrCode': (f"MarketData/MarketDataItem[@type='MARKETCAP']", 'currCode'),
+                '52WKHIGH_Unit': (f"MarketData/MarketDataItem[@type='52WKHIGH']", 'unit'),
+                '52WKHIGH_CurrCode': (f"MarketData/MarketDataItem[@type='52WKHIGH']", 'currCode'),
+                '52WKLOW_Unit': (f"MarketData/MarketDataItem[@type='52WKLOW']", 'unit'),
+                '52WKLOW_CurrCode': (f"MarketData/MarketDataItem[@type='52WKLOW']", 'currCode'),
+            },
+        }
+        columns = ['code'] + list(security_mappings['values'].keys()) + list(security_mappings['attributes'].keys())
+        column_data = {c: [] for c in columns}
+
+        for security in security_els:
+            for key, value in security_mappings['values'].items():
+                column_data[key] += [ e.text for e in security.findall(value)]
+                
+            for key, (value, attribute) in security_mappings['attributes'].items():
+                column_data[key] += [ e.attrib[attribute] for e in security.findall(value)]
+
+            column_data['code'] += [security.attrib['code']] * (len(column_data[key]) - len(column_data['code']))
+
+        # Fill dataframe:
+        df = pd.DataFrame(columns=columns)
+        for c in columns:
+            df[c] = column_data[c]
+        return df
 
     """
     2. Process company profile
     This should in the future done reversely: for all companies, get the company profile and store it in a single dataframe
     Currently however, it's done for a single company.
     """
-    def process_company_profile(xml_root):
-        company_el = xml_root.find('Company')
+    def process_company_profile(tree, ticker):
+        company_els = tree.findall('Company')
         column_mapping_companyInfo = {
             'values': {
                 'name': 'CoName/Name',
@@ -54,15 +98,23 @@ def process_RESC(xml_file):
                 'curFiscalPeriod_periodType': ('CompanyInfo/CurFiscalPeriod', 'periodType')
             },
         }
-        df = pd.DataFrame(columns=list(column_mapping_companyInfo['values'].keys()).append(column_mapping_companyInfo['attributes'].keys()))
 
-        df_loc = len(df)
+        columns = ['ticker'] + list(column_mapping_companyInfo['values'].keys()) + list(column_mapping_companyInfo['attributes'].keys())
+        column_data = {c: [] for c in columns}
 
-        for key, value in column_mapping_companyInfo['values'].items():
-            df.loc[df_loc, key] = company_el.find(value).text
+        for company in company_els:
+            for key, value in column_mapping_companyInfo['values'].items():
+                column_data[key] += [ e.text for e in company.findall(value)]
+                
+            for key, (value, attribute) in column_mapping_companyInfo['attributes'].items():
+                column_data[key] += [ e.attrib[attribute] for e in company.findall(value)]
+
+            column_data['ticker'] += [ticker] * (len(column_data[key]) - len(column_data['ticker']))
             
-        for key, (value, attribute) in column_mapping_companyInfo['attributes'].items():
-            df.loc[df_loc, key] = company_el.find(value).attrib[attribute]
+        # Fill dataframe:
+        df = pd.DataFrame(columns=columns)
+        for c in columns:
+            df[c] = column_data[c]
 
         return df
     """
@@ -129,69 +181,119 @@ def process_RESC(xml_file):
         # Fill dataframe:
         df = pd.DataFrame(columns=columns)
         for c in columns:
-            print("Inserting", c)
             df[c] = column_data[c]
         return df
     
-    def process_actuals_annual(xml_root, ticker):
-        return process_actuals_helper(xml_root, ticker, 'A')
+    def process_actuals_annual(tree, ticker):
+        return process_actuals_helper(tree, ticker, 'A')
 
     """
     4b. Process actuals (interim)
     """
-    def process_actuals_interim(xml_root, ticker):
-        return process_actuals_helper(xml_root, ticker, 'Q')
+    def process_actuals_interim(tree, ticker):
+        return process_actuals_helper(tree, ticker, 'Q')
     
     """
     5a. Fiscal Year Estimates 
     """
-    def process_fiscal_year_estimates_helper(xml_root, ticker, periodType='A'):
-        actuals = tree.findall(f"ConsEstimates/FYEstimates/FYEstimate")
-        actual_mappings = {
-            'values': {
-                'high_curr': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='High']/ConsValue[@dateType='CURR']",
-                'low_curr': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='Low']/ConsValue[@dateType='CURR']",
-                'mean_curr': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='Mean']/ConsValue[@dateType='CURR']",
-                'mean_1ma': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='Mean']/ConsValue[@dateType='1MA']",
-                'mean_3ma': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='Mean']/ConsValue[@dateType='3MA']",
-                'median_curr': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='Median']/ConsValue[@dateType='CURR']",
-                'stdev_curr': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='StdDev']/ConsValue[@dateType='CURR']",
-                'numberOfEst_curr': f"FYPeriod[@periodType='{periodType}']/ConsEstimate[@type='NumOfEst']/ConsValue[@dateType='CURR']",
-            }, 
-            'attributes': {
-                'fYear': (f"FYPeriod[@periodType='{periodType}']", 'fYear'),
-                'endMonth': (f"FYPeriod[@periodType='{periodType}']", 'endMonth'),
-                'endCalYear': (f"FYPeriod[@periodType='{periodType}']", 'endCalYear')
-            },
-        }
-        
-        # Create all the columns:
-        columns = ['ticker', 'type', 'unit'] + list(actual_mappings['values'].keys()) + list(actual_mappings['attributes'].keys())
+    def xml_processor_helper(toplevel_elts: list, mappings: dict, toplevel_attrs: dict = {}, extra_fixed_columns: dict = {}):
+        columns = [ a for a in toplevel_attrs.keys() ] + [ c for c in extra_fixed_columns.keys() ] + list(mappings['values'].keys()) + list(mappings['attributes'].keys())
         column_data = {c: [] for c in columns}
 
-        for fyactual in actuals:
-            for key, value in actual_mappings['values'].items():
-                column_data[key] += [ e.text for e in fyactual.findall(value)]
+        for el in toplevel_elts:
+            for key, value in mappings['values'].items():
+                column_data[key] += [ e.text for e in el.findall(value)]
                 
-            for key, (value, attribute) in actual_mappings['attributes'].items():
-                column_data[key] += [ e.attrib[attribute] for e in fyactual.findall(value)]
+            for key, (value, attribute) in mappings['attributes'].items():
+                column_data[key] += [ e.attrib[attribute] for e in el.findall(value)]
 
-            column_data['ticker'] += [ticker] * (len(column_data[key]) - len(column_data['ticker']))
-            column_data['type'] += [fyactual.attrib['type']] * (len(column_data[key]) - len(column_data['type']))
-            column_data['unit'] += [fyactual.attrib['unit']]  * (len(column_data[key]) - len(column_data['unit']))
+            for key, value in toplevel_attrs.items():
+                column_data[key] += [ el.attrib[value] ] * (len(column_data[key]) - max([len(a) for a in column_data.values()]))
+            
+            for key, value in extra_fixed_columns.items():
+                column_data[key] += [ value ] * (len(column_data[key]) - max([len(a) for a in column_data.values()]))
+        
+        return column_data
+    
+    def process_fiscal_year_estimates_helper(tree, ticker, periodType='A'):
+        fy_parentlevel_attrs = {'type': 'type', 'unit': 'unit'}
+        fy_extra_fixed_columns = {'ticker': ticker}
+        fy_mappings = {
+                    'values': {
+                        'high_curr': f"ConsEstimate[@type='High']/ConsValue[@dateType='CURR']",
+                        'low_curr': f"ConsEstimate[@type='Low']/ConsValue[@dateType='CURR']",
+                        'mean_curr': f"ConsEstimate[@type='Mean']/ConsValue[@dateType='CURR']",
+                        'mean_1ma': f"ConsEstimate[@type='Mean']/ConsValue[@dateType='1MA']",
+                        'mean_3ma': f"ConsEstimate[@type='Mean']/ConsValue[@dateType='3MA']",
+                        'median_curr': f"ConsEstimate[@type='Median']/ConsValue[@dateType='CURR']",
+                        'stdev_curr': f"ConsEstimate[@type='StdDev']/ConsValue[@dateType='CURR']",
+                        'numberOfEst_curr': f"ConsEstimate[@type='NumOfEst']/ConsValue[@dateType='CURR']",
+                    },
+                    'attributes': {}
+                }
+        fy_toplevelattribs = {
+                        'fYear': 'fYear',
+                        'endMonth': 'endMonth',
+                        'endCalYear': 'endCalYear'
+                    }
+        # Voor de helper stapt deze een niveau te hoog in. De FYEstimate heeft namelijk per FYPeriod verschillende aantallen waarden.
+        # Daarom halen we eerst de algemene attributen van FYEstimate op. Daarna gaan we een niveau dieper.
+        fyestimates = tree.findall(f"ConsEstimates/FYEstimates/FYEstimate")
+        columns = [ a for a in fy_parentlevel_attrs.keys() ] + [ c for c in fy_extra_fixed_columns.keys() ] + list(fy_mappings['values'].keys()) + list(fy_toplevelattribs.keys())
+        column_data = {c: [] for c in columns}
 
-        # Fill dataframe:
-        df = pd.DataFrame(columns=columns)
-        for c in columns:
-            print("Inserting", c)
-            df[c] = column_data[c]
-        return df
+        for f in fyestimates:
+            extra_fixed_columns = {'ticker': ticker, 'type': f.attrib['type'], 'unit': f.attrib['unit']}
+            fyperiods = f.findall(f"FYPeriod[@periodType='{periodType}']")
+            estimate_data = xml_processor_helper(fyperiods, fy_mappings, fy_toplevelattribs, extra_fixed_columns)
+            for c in columns:
+                column_data[c] += list(estimate_data[c])
+
+        
+
+    def process_fiscal_year_estimates_annual(tree, ticker):
+        return process_fiscal_year_estimates_helper(tree, ticker, 'A')
+
+    def process_fiscal_year_estimates_interim(tree, ticker):
+        return process_fiscal_year_estimates_helper(tree, ticker, 'Q')
 
     """
     5b. Net Profit Estimates
     """
-    def process_net_profit_estimates(xml_root, ticker):
-        pass
+    def process_net_profit_estimates(tree, ticker):
+        estimates = tree.findall(f"ConsEstimates/NPEstimates/NPEstimate")
+        estimate_mappings = {
+            'values': {
+                'high_curr': f"NPEstimate/ConsEstimate[@type='High']/ConsValue[@dateType='CURR']",
+                'low_curr': f"NPEstimate/ConsEstimate[@type='Low']/ConsValue[@dateType='CURR']",
+                'mean_curr': f"NPEstimate/ConsEstimate[@type='Mean']/ConsValue[@dateType='CURR']",
+                'median_curr': f"NPEstimate/ConsEstimate[@type='Median']/ConsValue[@dateType='CURR']",
+            },
+            'attributes': {}
+        }
+        
+        columns = ['ticker', 'type', 'unit'] + list(estimate_mappings['values'].keys()) + list(estimate_mappings['attributes'].keys())
+        column_data = {c: [] for c in columns}
+
+        for npestimate in estimates:
+            for key, value in estimate_mappings['values'].items():
+                column_data[key] += [ e.text for e in npestimate.findall(value)]
+                
+            for key, (value, attribute) in estimate_mappings['attributes'].items():
+                column_data[key] += [ e.attrib[attribute] for e in npestimate.findall(value)]
+
+            column_data['ticker'] += [ticker] * (len(column_data[key]) - len(column_data['ticker']))
+            column_data['type'] += [npestimate.attrib['type']] * (len(column_data[key]) - len(column_data['type']))
+            column_data['unit'] += [npestimate.attrib['unit']]  * (len(column_data[key]) - len(column_data['unit']))
+
+        # Fill dataframe:
+        df = pd.DataFrame(columns=columns)
+        for c in columns:
+            df[c] = column_data[c]
+        return df
+
+    # Process the XML file
+    process_company_profile(tree)
 
 def process_ReportSnapshot(xml_file):
     return
